6e4d59e507a18fb9f34d2bf33f8db8fab3006226
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-11-26 09:56:04 +0600

    popular languages percentage

--- sof-demo.py
+++ sof-demo.py
@@ -6,6 +6,7 @@ from collections import defaultdict, Counter
 
 with open('data/survey_results_public.csv', encoding="utf8") as f:
     csv_reader = csv.DictReader(f)
+    total = 0
 
     language_counter = Counter()
 
@@ -14,7 +15,12 @@ with open('data/survey_results_public.csv', encoding="utf8") as f:
 
         language_counter.update(languages)
 
-print(language_counter.most_common(10))
+        total += 1
+for language, value in language_counter.most_common(10):
+    language_pct = (value / total) * 100
+    language_pct = round(language_pct, 2)
+
+    print(f'{language}: {language_pct}%')
 
 
 
